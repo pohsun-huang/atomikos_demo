014253be211b7ae7820f8e654c0e6fc5f6fecd1b
Po-Hsun <pohsun@infinitiesoft.com>
2016-04-04 14:10:46 +0800

    update hibernate version to 4.3.5.Final

---
+++ src/main/java/demo/platform/SpringJtaPlatformAdapter.java
@@ -0,0 +1,49 @@
+package demo.platform;
+
+import javax.transaction.TransactionManager;
+import javax.transaction.UserTransaction;
+
+import org.hibernate.engine.transaction.jta.platform.internal.AbstractJtaPlatform;
+import org.springframework.transaction.jta.JtaTransactionManager;
+import org.springframework.util.Assert;
+
+public class SpringJtaPlatformAdapter extends AbstractJtaPlatform {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static TransactionManager sTransactionManager;
+    private static UserTransaction sUserTransaction;
+
+
+    @Override
+    protected TransactionManager locateTransactionManager() {
+        Assert.notNull(sTransactionManager, "TransactionManager has not been setted");
+        return sTransactionManager;
+    }
+
+
+    @Override
+    protected UserTransaction locateUserTransaction() {
+        Assert.notNull(sUserTransaction, "UserTransaction has not been setted");
+        return sUserTransaction;
+    }
+
+
+    public void setJtaTransactionManager(JtaTransactionManager jtaTransactionManager) {
+        sTransactionManager = jtaTransactionManager.getTransactionManager();
+        sUserTransaction = jtaTransactionManager.getUserTransaction();
+    }
+
+
+    public void setTransactionManager(TransactionManager transactionManager) {
+        sTransactionManager = transactionManager;
+    }
+
+
+    public void setUserTransaction(UserTransaction userTransaction) {
+        sUserTransaction = userTransaction;
+    }
+
+}
